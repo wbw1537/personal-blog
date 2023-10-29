fb721991ed7b1174df240aa5bf0e9f3215d7c792
wang bowen <wbw1537@qq.com>
2023-10-29 23:56:29 +0800

    实现登录功能
    参考视频：https://www.bilibili.com/video/BV1hq4y1F7zk/?p=35&share_source=copy_web&vd_source=e577d7ea13b55424ecb5858eebe248bc

---
+++ wbwBlog/wbw-frameWork/src/main/java/com/wbw1537/service/BlogLoginService.java
@@ -0,0 +1,10 @@
+package com.wbw1537.service;
+
+import com.wbw1537.domain.ResponseResult;
+import com.wbw1537.domain.entity.User;
+import org.springframework.stereotype.Service;
+
+
+public interface BlogLoginService {
+    ResponseResult login(User user);
+}
---
+++ wbwBlog/wbw-frameWork/src/main/java/com/wbw1537/service/impl/BlogLoginServiceImpl.java
@@ -0,0 +1,50 @@
+package com.wbw1537.service.impl;
+
+import com.mysql.cj.log.Log;
+import com.wbw1537.domain.ResponseResult;
+import com.wbw1537.domain.entity.LoginUser;
+import com.wbw1537.domain.entity.User;
+import com.wbw1537.domain.vo.BlogUserLoginVo;
+import com.wbw1537.domain.vo.UserInfoVo;
+import com.wbw1537.service.BlogLoginService;
+import com.wbw1537.utils.BeanCopyUtils;
+import com.wbw1537.utils.JwtUtil;
+import com.wbw1537.utils.RedisCache;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class BlogLoginServiceImpl implements BlogLoginService {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private RedisCache redisCache;
+    @Override
+    public ResponseResult login(User user) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user.getUserName(),user.getPassword());
+        // 调用UserDetailsService
+        Authentication authenticate = authenticationManager.authenticate(authenticationToken);
+        // 判断是否认证通过
+        if(Objects.isNull(authenticate)){
+            throw new RuntimeException("用户名或密码错误");
+        }
+        //获取userId生成token
+        LoginUser loginUser = (LoginUser) authenticate.getPrincipal();
+        String userId = loginUser.getUser().getId().toString();
+        String jwt = JwtUtil.createJWT(userId);
+        // 把用户信息存入redis
+        redisCache.setCacheObject("bloglogin:"+userId,loginUser);
+        // 将user bean拷贝为userInfoVo
+        UserInfoVo userInfoVo = BeanCopyUtils.copyBean(loginUser.getUser(), UserInfoVo.class);
+        // 将token和userInfo封装返回给前端
+        BlogUserLoginVo vo = new BlogUserLoginVo(jwt,userInfoVo);
+        return ResponseResult.okResult(vo);
+    }
+}
