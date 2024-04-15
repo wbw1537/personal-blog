288411fb5cf1acf421515eedc072949c9fda4280
wbw1537 <wbw1537@qq.com>
2024-04-15 11:35:09 +0800

    rebooting

--- wbwBlog/wbw-frameWork/src/main/java/com/wbw1537/mapper/ArticleMapper.java
+++
@@ -1,7 +0,0 @@
-package com.wbw1537.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.wbw1537.domain.entity.Article;
-
-public interface ArticleMapper extends BaseMapper<Article> {
-}
--- wbwBlog/wbw-frameWork/src/main/java/com/wbw1537/service/UploadService.java
+++
@@ -1,9 +0,0 @@
-package com.wbw1537.service;
-
-import com.wbw1537.domain.ResponseResult;
-import com.wbw1537.domain.dto.UserInfoDto;
-import org.springframework.web.multipart.MultipartFile;
-
-public interface UploadService{
-    ResponseResult uploadImg(MultipartFile img);
-}
